d51b448a8fa0c7e7be301734b6069aba631735ec
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-10-21 12:18:38 +0330

    feat: handle error situation

--- src/APIs/destinations.ts
+++ src/APIs/destinations.ts
@@ -2,18 +2,23 @@ import { IDestinations } from "../models/destinations";
 
 export const fetchDestinations = async (
   input: string
-): Promise<IDestinations[]> => {
+): Promise<IDestinations[] | { errorMessage: string }> => {
   console.log(
     "Approximate Endpoint : ",
     // eslint-disable-next-line no-template-curly-in-string
     "`${SERVER_URL}/destinations?filter[name][_contains]=${input}`"
   );
   try {
+    if (input.toLocaleLowerCase() === "fail") {
+      throw new Error();
+    }
     const response = await fetch("/destinations.json");
     const destArray = await response.json();
     return destArray.filter((dest: IDestinations) => dest.name.includes(input));
   } catch (error) {
     console.error("fetching destinations failed");
-    return [];
+    return {
+      errorMessage: "fetching destinations failed",
+    };
   }
 };
