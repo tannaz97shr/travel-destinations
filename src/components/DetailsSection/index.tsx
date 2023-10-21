a8ebd8cc60b800db50a623955c3c7ad1d66aa32e
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-10-21 19:56:38 +0330

    feat: complette display details

--- src/components/DetailsSection/index.tsx
+++ src/components/DetailsSection/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { fetchDestinationById } from "../../APIs/destinations";
+import Info from "./Info";
 
 function Details() {
   let { destinationId } = useParams();
@@ -11,7 +12,23 @@ function Details() {
   });
   console.log("query", data);
   console.log("Details section", destinationId);
-  return <div>details section</div>;
+  return (
+    <div className="flex flex-col mt-6 p-4 border border-custom-pink rounded-lg">
+      <div className="font-bold text-custom-yellow text-lg mb-3">
+        {data?.destination?.name}
+      </div>
+      <div className="mb-3">{data?.destination?.description}</div>
+      {data?.destination?.country && (
+        <Info label={"country"} value={data?.destination?.country} />
+      )}
+      {data?.destination?.climate && (
+        <Info label={"climate"} value={data?.destination?.climate} />
+      )}
+      {data?.destination?.currency && (
+        <Info label={"currency"} value={data?.destination?.currency} />
+      )}
+    </div>
+  );
 }
 
 export default Details;
